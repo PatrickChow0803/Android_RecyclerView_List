da8ea54be78e530cbd999969517ddffab54e46fb
Patrick Chow <PatrickChow0803@gmail.com>
2019-08-13 16:39:36 -0400

    Completed the List Item Layout step

---
+++ app/src/main/java/com/patrickchow/booksrecyclerview/model/Book.kt
@@ -0,0 +1,5 @@
+package com.patrickchow.booksrecyclerview.model
+
+import android.graphics.drawable.Drawable
+
+class Book (title: String, author: String, cover: Drawable)
--- app/src/main/java/com/patrickchow/booksrecyclerview/ui/ListActivity.kt
+++ app/src/main/java/com/patrickchow/booksrecyclerview/ui/ListActivity.kt
@@ -1,9 +1,10 @@
-package com.patrickchow.booksrecyclerview
+package com.patrickchow.booksrecyclerview.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.patrickchow.booksrecyclerview.R
 
-class MainActivity : AppCompatActivity() {
+class ListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
